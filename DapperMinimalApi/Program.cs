0d00f72f615595dbcedd2d16888ee9d7990e172b
Mike Hansford <mjhansford@gmail.com>
2025-07-03 18:07:13 +1000

    update prior to rebase

--- DapperMinimalApi/Program.cs
+++ DapperMinimalApi/Program.cs
@@ -1,3 +1,7 @@
+using System.Security.Cryptography;
+using DapperMinimalApi;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,20 +25,15 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
+app.MapGet("/weatherforecast", () => "Hello weather forecast" )
     .WithName("GetWeatherForecast")
     .WithOpenApi();
+
+app.MapGet("/search/{name}", (string name) => $"Hello {name}");
+
+app.MapGet("/search", ([FromQuery(Name="q")]string text) => $"Hi {text}");
+
+PeopleHandler.MapEndpoints(app);
 
 app.Run();
 
